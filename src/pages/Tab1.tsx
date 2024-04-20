3d6f466de1575eab6b8d8ed0b9461e8407b8e9d9
sebastien <miantamag@gmail.com>
2024-04-20 15:40:20 +0200

    joke chuck norris

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,22 +1,46 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
-import './Tab1.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonButton } from '@ionic/react';
 
 const Tab1: React.FC = () => {
+  const [joke, setJoke] = useState<string>('');
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.get('https://api.chucknorris.io/jokes/random');
+      if (response.data && response.data.value) {
+        setJoke(response.data.value);
+      }
+    } catch (error) {
+      console.error('Error fetching joke:', error);
+    }
+  };
+
+  useEffect(() => {
+    fetchData(); // Appel fetchData au chargement initial de la page
+  }, []);
+
+  const handleFetchJoke = () => {
+    fetchData(); // Appel fetchData lorsque le bouton est cliqué
+  };
+
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tab 1</IonTitle>
+          <IonTitle>Random Joke</IonTitle>
         </IonToolbar>
       </IonHeader>
-      <IonContent fullscreen>
+      <IonContent fullscreen className="ion-text-center">
         <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Tab 1</IonTitle>
+            <IonTitle size="large">random Joke</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer name="Tab 1 page" />
+        <div className="ion-padding">
+          <p>{joke}</p>
+          <IonButton onClick={handleFetchJoke}>New Joke</IonButton> {/* Bouton pour récupérer une nouvelle blague */}
+        </div>
       </IonContent>
     </IonPage>
   );
--- src/pages/Tab2.tsx
+++ src/pages/Tab2.tsx
@@ -1,22 +1,56 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
-import './Tab2.css';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { IonButton, IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 
 const Tab2: React.FC = () => {
+  const [joke, setJoke] = useState<string>('');
+
+  const fetchDataCategory = async (category: string) => {
+    try {
+      const response = await axios.get(`https://api.chucknorris.io/jokes/random?category=${category}`);
+      if (response.data && response.data.value) {
+        setJoke(response.data.value);
+      }
+    } catch (error) {
+      console.error('Error fetching joke:', error);
+    }
+  };
+
+  useEffect(() => {
+    // Fetch a random joke initially when component mounts
+    fetchDataCategory('animal');
+  }, []); // Empty dependency array means this effect runs only once on mount
+
+  const handleFetchJoke = (category: string) => {
+    fetchDataCategory(category);
+  };
+
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tab 2</IonTitle>
+          <IonTitle>Joke By Category</IonTitle>
         </IonToolbar>
       </IonHeader>
-      <IonContent fullscreen>
+      <IonContent fullscreen className="ion-text-center">
         <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Tab 2</IonTitle>
+            <IonTitle size="large">Joke By Category</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer name="Tab 2 page" />
+        <div className="ion-padding">
+          <p>{joke}</p>
+          <IonButton onClick={() => handleFetchJoke('animal')}>Animal</IonButton>
+          <IonButton onClick={() => handleFetchJoke('celebrity')}>Celebrity</IonButton>
+          <IonButton onClick={() => handleFetchJoke('career')}>Career</IonButton>
+          <IonButton onClick={() => handleFetchJoke('money')}>Money</IonButton>
+          <IonButton onClick={() => handleFetchJoke('sport')}>Sport</IonButton>
+          <IonButton onClick={() => handleFetchJoke('political')}>Political</IonButton>
+          <IonButton onClick={() => handleFetchJoke('history')}>History</IonButton>
+          <IonButton onClick={() => handleFetchJoke('food')}>Food</IonButton>
+          <IonButton onClick={() => handleFetchJoke('science')}>Science</IonButton>
+          <IonButton onClick={() => handleFetchJoke('religion')}>Religion</IonButton>
+        </div>
       </IonContent>
     </IonPage>
   );
